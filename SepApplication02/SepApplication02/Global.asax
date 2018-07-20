64a4c4243bb2d73c45754943824696ac3eb41ed1
unknown <ducminhxd2011@gmail.com>
2018-07-20 16:38:18 +0700

    empty web

---
+++ SepApplication02/SepApplication02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SepApplication02.MvcApplication" Language="C#" %>
---
+++ SepApplication02/SepApplication02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SepApplication02.Startup))]
+namespace SepApplication02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
